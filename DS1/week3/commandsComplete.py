46ac9fa93f58e62543b29e853f107f18bacac3e0
Rommel Villagomez <rommelvillagomez@Rommels-MacBook-Pro.local>
2017-08-12 00:28:03 -0500

    more problems solved

--- DS1/week3/commandsComplete.py
+++ DS1/week3/commandsComplete.py
@@ -37,3 +37,63 @@ columns_to_keep = ['Rank', 'Documents', 'Citable documents', 'Citations', 'Self-
 df = df1[columns_to_keep]
 df = df.loc[df['Rank'] < 16]
 df.loc[:,'Rank'] += 1
+
+#number of rows that were lost
+len(df1) - len(df) 
+
+# What is the average GDP over the last 10 years for each country? (exclude missing values from this calculation.)
+# This function should return a Series named avgGDP with 15 countries and their average GDP sorted in descending order.
+
+avgGDP = df[['2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015']].mean(axis=1).sort_values(ascending=False)
+
+#By how much had the GDP changed over the 10 year span for the country with the 6th largest average GDP?
+avgGDPCountry = df[['2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015']].mean(axis=1).sort_values(ascending=False).index[5]
+df.get_value(avgGDPCountry,'2015') - df.get_value(avgGDPCountry,'2006')
+
+#What is the mean Energy Supply per Capita?
+meanESpC = df[['Energy Supply per Capita']].mean(axis=0).values[0]
+
+#What country has the maximum % Renewable and what is the percentage?
+list(zip(df[['% Renewable']].idxmax(axis=0),df[['% Renewable']].max(axis=0)))
+
+# Create a new column that is the ratio of Self-Citations to Total Citations. 
+# What is the maximum value for this new column, and what country has the highest ratio?
+df['Citations Ratio'] = df['Self-citations']/df['Citations']
+list(zip(df[['Citations Ratio']].idxmax(axis=0),df[['Citations Ratio']].max(axis=0)))
+
+# Create a column that estimates the population using Energy Supply and Energy Supply per capita. 
+# What is the third most populous country according to this estimate?
+df['Population Estimate'] = df['Energy Supply']/df['Energy Supply per Capita']
+thirdCountryPop = df[['Population Estimate']].max(axis=1).sort_values(ascending=False).index[2]
+
+# Create a column that estimates the number of citable documents per person. 
+# What is the correlation between the number of citable documents per capita and the energy supply per capita? 
+# Use the .corr() method, (Pearson's correlation).
+df['Citable docs per Capita'] = df['Citable documents'] / df['Population Estimate']
+df['Citable docs per Capita'].corr(df['Energy Supply per Capita'])
+
+# Create a new column with a 1 if the country's % Renewable value is at or above the median 
+# for all countries in the top 15, and a 0 if the country's % Renewable value is below the median.
+# This function should return a series named HighRenew whose index is 
+# the country name sorted in ascending order of rank.
+medianRenew = df[['% Renewable']].median(axis=0).values[0]
+df['Median Renew'] = np.where(df['% Renewable'] >= medianRenew, 1, 0)
+qq = df['Rank'].max(axis=1).sort_values(ascending=False)
+
+
+ContinentDict  = {'China':'Asia', 
+                  'United States':'North America', 
+                  'Japan':'Asia', 
+                  'United Kingdom':'Europe', 
+                  'Russian Federation':'Europe', 
+                  'Canada':'North America', 
+                  'Germany':'Europe', 
+                  'India':'Asia',
+                  'France':'Europe', 
+                  'South Korea':'Asia', 
+                  'Italy':'Europe', 
+                  'Spain':'Europe', 
+                  'Iran':'Asia',
+                  'Australia':'Australia', 
+                  'Brazil':'South America'}
+df['Continent'] = pd.Series(ContinentDict)
