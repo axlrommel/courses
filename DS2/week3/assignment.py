5dd77e788f970d32b6a4bea58b75484eea39aa34
Rommel Villagomez <rommelvillagomez@Rommels-MacBook-Pro.local>
2017-09-08 21:49:56 -0500

    updated homework

--- DS2/week3/assignment.py
+++ DS2/week3/assignment.py
@@ -25,18 +25,29 @@ labels = df3.index
 data = df3['mean']
 error = df3['95_conf']
 
+font = {'family': 'serif',
+        'color':  'darkred',
+        'weight': 'normal',
+        'size': 10,
+        }
+
 width = 0.5
-threshold = 38000
+threshold = 38500
 fig = plt.figure()
 ax = fig.add_subplot(1,1,1)
 xlocations = np.array(range(len(data)))+width
-barlist = ax.bar(xlocations, data, yerr=error, width=width, error_kw=dict(lw=1, capsize=10, capthick=1))
+barlist = ax.bar(xlocations, data, yerr=error, width=width, 
+    error_kw=dict(lw=1, capsize=10, capthick=1))
 barlist[0].set_color('r')
+barlist[1].set_color('b')
+barlist[2].set_color('#33ffec')
+barlist[3].set_color('b')
 ax.set_xticks(xlocations)
 ax.set_xticklabels(labels)
-ax.set_xlim(0, xlocations[-1]+width*2)
-plt.title("Average Ratings on the Training Set")
-ax.plot([0., 4.5], [threshold, threshold], "k--")
+ax.set_xlim(0, xlocations[-1]+ width)
+ax.text(-.44, threshold - 800, str(threshold), font)
+plt.title("Assignment Week 3")
+ax.plot([0., 4.5], [threshold, threshold], "r--")
 plt.gca().get_xaxis().tick_bottom()
 plt.gca().get_yaxis().tick_left()
 plt.show()
